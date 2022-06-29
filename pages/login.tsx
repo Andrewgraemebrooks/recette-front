8e48529a769752f669593ed3cee244a6b39fe4b6
Andrew Brooks <25846476+Andrewgraemebrooks@users.noreply.github.com>
2022-06-29 20:47:51 +0100

    Started work on the mobile view of the login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,22 @@
 import type { NextPage } from "next";
 
 const Login: NextPage = () => {
+  // Example: https://tailwindcomponents.com/component/mobile-login-page
   return (
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
+    <div className="flex flex-col border-solid border-2 min-h-screen">
+      <h1>Login</h1>
+      <div className="grow flex flex-col justify-center border-solid border-2">
+        <div className="border-solid border-2">
+          <label>Email</label>
+          <input type="email" />
+        </div>
+        <div className="border-solid border-2">
+          <label>Password</label>
+          <input type="password" />
+        </div>
+      </div>
+      <button>Log in</button>
+    </div>
   )
 }
 
