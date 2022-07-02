a46f5bf22b4a76abf900289626863cf427ef5c41
Andrew Brooks <25846476+Andrewgraemebrooks@users.noreply.github.com>
2022-07-02 15:02:52 +0100

    Finished mobile view of the login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,21 +1,58 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next'
 
 const Login: NextPage = () => {
-  // Example: https://tailwindcomponents.com/component/mobile-login-page
   return (
     <div className="flex flex-col border-solid border-2 min-h-screen">
-      <h1>Login</h1>
-      <div className="grow flex flex-col justify-center border-solid border-2">
-        <div className="border-solid border-2">
-          <label>Email</label>
-          <input type="email" />
-        </div>
-        <div className="border-solid border-2">
-          <label>Password</label>
-          <input type="password" />
-        </div>
+      <div className="title flex justify-center">
+        <h1 className="text-2xl">Recette</h1>
       </div>
-      <button>Log in</button>
+      <form className="flex items-center flex-col flex-grow justify-center bg-white shadow-md rounded px-8 pt-6 pb-8">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Username"
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="******************"
+          />
+          <p className="text-red-500 text-xs italic">
+            Please choose a password.
+          </p>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-4"
+            type="button"
+          >
+            Sign In
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            href="#"
+          >
+            Forgot Password?
+          </a>
+        </div>
+      </form>
     </div>
   )
 }
